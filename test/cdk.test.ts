0a660fef758aced9da88aa3d65b0789f9f6d870a
Takapon0407 <takapon_business@yahoo.co.jp>
2024-11-23 17:32:59 +0900

    前回のCDK実行時のリソースと比較テストをおこなうための設定を追加

--- test/cdk.test.ts
+++ test/cdk.test.ts
@@ -1,17 +1,25 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as Cdk from '../lib/cdk-stack';
+import * as cdk from "aws-cdk-lib";
+import { Template } from "aws-cdk-lib/assertions";
+import { CdkStack } from "../lib/stack/cdk-stack";
+import { AppStack } from "../lib/stack/app-stack";
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/cdk-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new Cdk.CdkStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+test(`Matches the snapshot cdkStack`, () => {
+  const app = new cdk.App();
+  expect(
+    Template.fromStack(
+      new CdkStack(app, "CdkStack", {
+        env: {
+          account: "945611748417",
+          region: "ap-northeast-1",
+        },
+      })
+    ).toJSON()
+  ).toMatchSnapshot();
+});
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+test(`Matches the snapshot appStack`, () => {
+  const app = new cdk.App();
+  expect(
+    Template.fromStack(new AppStack(app, "AppStack")).toJSON()
+  ).toMatchSnapshot();
 });
